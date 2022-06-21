14eb047219bc7cc320fb5ff6dfa5f9bef27e8cf1
Keith Heggart <admin@eduation4u.com.au>
2022-06-22 08:45:43 +1000

    Updated explanation and added images

--- imageViewer(HWS1)/ViewController.swift
+++ imageViewer(HWS1)/ViewController.swift
@@ -4,6 +4,9 @@
 //
 //  Created by Keith Heggart on 22/6/2022.
 //
+// This project is intented to display 10 images, with text below them. It will use a table view controller, and then a detail view controller to display the image and the text.
+// The information will be hard coded and then stored in the file structure, where it will be accessed. I WON'T make them objects as such, but instead numbered arrays.
+//The key thing will be passign the information between the two view controllers. This is something that I want to practice.
 
 import UIKit
 
