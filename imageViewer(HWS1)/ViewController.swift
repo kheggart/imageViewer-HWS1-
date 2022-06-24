8cf2ae90c6b5fd43896288d373729f65e91d8191
Keith Heggart <admin@eduation4u.com.au>
2022-06-24 11:19:51 +1000

    Changed to TableView, added a new TableViewController, added an identifier to the cell, embedded in navigation controller, and added the table view controller methods.

--- imageViewer(HWS1)/ViewController.swift
+++ imageViewer(HWS1)/ViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 
 var pictures = [String]()
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
 
 
@@ -35,6 +35,21 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 
 
 }
